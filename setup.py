7171835f8a99e35f3fd9df11668e6b7142541b3a
protogia <goldengrizz01@gmail.com>
2023-12-26 19:17:00 +0100

    increase pip-version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="govdata",
     author="Giancarlo Rizzo",
-    version="0.0.7",
+    version="0.0.8",
     packages=["."],
     install_requires=[
         'Jinja2==3.0.3',
